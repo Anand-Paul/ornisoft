f017a37a304205eaffee5b38ca75cb95f143b77a
Anand-Paul <anandmalakkaran@gmail.com>
2022-10-22 18:43:15 +0530

    refactoring

--- src/pages/product-db-analyzer.js
+++ src/pages/product-db-analyzer.js
@@ -1,9 +1,26 @@
 import * as React from "react";
 
 import Layout from "../components/layout";
+import Breadcrumb from "../components/breadcrumb";
 
 const ProductDBAnalyzer = () => {
-  return <Layout></Layout>;
+  const breadcrumbData = [
+    {
+      id: 1,
+      name: "Home",
+      link: "/",
+    },
+    {
+      id: 2,
+      name: "DB Analyzer",
+    },
+  ];
+
+  return (
+    <Layout>
+      <Breadcrumb data={breadcrumbData} />
+    </Layout>
+  );
 };
 
 export default ProductDBAnalyzer;
--- src/pages/product-zcoder.js
+++ src/pages/product-zcoder.js
@@ -1,9 +1,26 @@
 import * as React from "react";
 
 import Layout from "../components/layout";
+import Breadcrumb from "../components/breadcrumb";
 
 const ProductZCODER = () => {
-  return <Layout></Layout>;
+  const breadcrumbData = [
+    {
+      id: 1,
+      name: "Home",
+      link: "/",
+    },
+    {
+      id: 2,
+      name: "ZCODER",
+    },
+  ];
+
+  return (
+    <Layout>
+      <Breadcrumb data={breadcrumbData} />
+    </Layout>
+  );
 };
 
 export default ProductZCODER;
