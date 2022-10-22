c58e3b456a1f7d8a56d5382fe53c78b7b6a6065b
Anand-Paul <anandmalakkaran@gmail.com>
2022-10-22 20:24:00 +0530

    minor changes

--- src/components/hero-slider/hero-slider.js
+++ src/components/hero-slider/hero-slider.js
@@ -1,67 +1,80 @@
-import { StaticImage } from "gatsby-plugin-image";
 import React from "react";
-// import { LazyLoadImage } from "react-lazy-load-image-component";
-
+import { StaticImage } from "gatsby-plugin-image";
 import Slider from "react-slick";
+
 import Banner from "../../images/banner.png";
 
 import "./hero-slider.scss";
 
 const HeroSlider = () => {
-    const settings = {
-        infinite: true,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        arrows: false,
-        dots: true,
-        autoplay: false,
-        autoplaySpeed: 20000,
-    }
+  const settings = {
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    dots: true,
+    autoplay: false,
+    autoplaySpeed: 3000,
+  };
 
-    return (
-        <div className="relative">
-            <div className="hero-slider">
-                <Slider {...settings}>
-                    <div className="hero-slider__slide">
-                        <div className="hero-slider__content">
-                            <div className="hero-slider__dialog">
-                                <h2 className="hero-slider__title">Product Heading Here</h2>
-                                <p className="hero-slider__description">
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vitae enim aenean sed nunc, euismod in velit malesuada. Duis faucibus morbi consectetur iaculis. In dui urna placerat ut sem imperdiet ultrices urna sollicitudin. Viverra semper quam risus sed magna.</p>
-                            </div>
-                            <StaticImage
-                                src={Banner}
-                                alt="banner"
-                                placeholder="blurred"
-                                layout="fixed"
-                                width={100}
-                                height={100}
-                                className="hero-slider__image"
-
-                            />
-                        </div>
-                    </div>
-                    <div className="hero-slider__slide">
-                        <div className="hero-slider__content">
-                            <div className="hero-slider__dialog">
-                                <h2 className="hero-slider__title">Product Heading Here</h2>
-                                <p className="hero-slider__description">
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vitae enim aenean sed nunc, euismod in velit malesuada. Duis faucibus morbi consectetur iaculis. In dui urna placerat ut sem imperdiet ultrices urna sollicitudin. Viverra semper quam risus sed magna.</p>
-                            </div>
-                        </div>
-                    </div> <div className="hero-slider__slide">
-                        <div className="hero-slider__content">
-                            <div className="hero-slider__dialog">
-                                <h2 className="hero-slider__title">Product Heading Here</h2>
-                                <p className="hero-slider__description">
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vitae enim aenean sed nunc, euismod in velit malesuada. Duis faucibus morbi consectetur iaculis. In dui urna placerat ut sem imperdiet ultrices urna sollicitudin. Viverra semper quam risus sed magna.</p>
-                            </div>
-                        </div>
-                    </div>
-
-                </Slider>
-            </div></div>
-    )
-
-}
+  return (
+    <div className="relative">
+      <div className="hero-slider">
+        <Slider {...settings}>
+          <div className="hero-slider__slide">
+            <div className="hero-slider__content">
+              <div className="hero-slider__dialog">
+                <h2 className="hero-slider__title">Product Heading Here</h2>
+                <p className="hero-slider__description">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vitae
+                  enim aenean sed nunc, euismod in velit malesuada. Duis
+                  faucibus morbi consectetur iaculis. In dui urna placerat ut
+                  sem imperdiet ultrices urna sollicitudin. Viverra semper quam
+                  risus sed magna.
+                </p>
+              </div>
+              <StaticImage
+                src={Banner}
+                alt="banner"
+                placeholder="blurred"
+                layout="fixed"
+                width={100}
+                height={100}
+                className="hero-slider__image"
+              />
+            </div>
+          </div>
+          <div className="hero-slider__slide">
+            <div className="hero-slider__content">
+              <div className="hero-slider__dialog">
+                <h2 className="hero-slider__title">Product Heading Here</h2>
+                <p className="hero-slider__description">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vitae
+                  enim aenean sed nunc, euismod in velit malesuada. Duis
+                  faucibus morbi consectetur iaculis. In dui urna placerat ut
+                  sem imperdiet ultrices urna sollicitudin. Viverra semper quam
+                  risus sed magna.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="hero-slider__slide">
+            <div className="hero-slider__content">
+              <div className="hero-slider__dialog">
+                <h2 className="hero-slider__title">Product Heading Here</h2>
+                <p className="hero-slider__description">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vitae
+                  enim aenean sed nunc, euismod in velit malesuada. Duis
+                  faucibus morbi consectetur iaculis. In dui urna placerat ut
+                  sem imperdiet ultrices urna sollicitudin. Viverra semper quam
+                  risus sed magna.
+                </p>
+              </div>
+            </div>
+          </div>
+        </Slider>
+      </div>
+    </div>
+  );
+};
 export default HeroSlider;
