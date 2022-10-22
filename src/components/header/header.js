ab9ac804ba51852cf6e069c0ae073dcb653491c4
sarathcr <sarathcr9177725@gmail.com>
2022-10-22 11:42:06 +0530

    updated sticky header

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -13,7 +13,7 @@ import youtubeIcon from "../../images/logo--youtube.png";
 
 const Header = () => {
   return (
-    <header className="header py-3">
+    <header className="header py-3 bg-white">
       <div className="container d-flex align-items-center justify-content-between">
         <a href="#">
           <img src={logo} alt="Logo" />
@@ -83,65 +83,7 @@ const Header = () => {
           </li>
         </ul>
       </div>
-      <nav class="navbar navbar-expand-lg navbar-light bg-light header__nav">
-        <div class="container">
-          <div className="d-flex align-items-center justify-content-between header__nav-wrapper">
-            <button
-              class="navbar-toggler d-block d-lg-none"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#navbarNav"
-              aria-controls="navbarNav"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span class="navbar-toggler-icon"></span>
-            </button>
-            <div className="header__search ps-4 d-flex d-lg-none align-items-center">
-              <div class="input-group flex-nowrap">
-                <input
-                  className="header__input shadow-none border-bottom"
-                  type="text"
-                  class="form-control"
-                />
-                <span className="header__icon">
-                  <img src={searchIcon} alt="search icon" />
-                </span>
-              </div>
-            </div>
-          </div>
 
-          <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav w-100 justify-content-center my-4 my-lg-0">
-              <li class="nav-item me-lg-3">
-                <a class="nav-link active" aria-current="page" href="#">
-                  Home
-                </a>
-              </li>
-              <li class="nav-item me-lg-3">
-                <a class="nav-link" href="#">
-                  About Us
-                </a>
-              </li>
-              <li class="nav-item me-lg-3">
-                <a class="nav-link" href="#">
-                  Products
-                </a>
-              </li>
-              <li class="nav-item me-lg-3">
-                <a class="nav-link" href="#">
-                  Solutions
-                </a>
-              </li>
-              <li class="nav-item me-lg-3">
-                <a class="nav-link" href="#">
-                  Contact Us
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
     </header>
   );
 };
